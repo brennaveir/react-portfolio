4fe6d08d57fcf9e1d72b98ee4fc7d1c077346ac7
brennaveir <brennaveir@hotmail.com>
2023-04-24 18:09:55 -0400

    added real content to about me paragraph

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,20 +5,42 @@ export default function AboutMe() {
   return (
     <div className="sectionContainer">
       <div className="aboutContainer d-flex flex-column align-items-center">
-    <Image className="aboutMePic m-5"src="./porfolio-pic2.jpg"  alt="Brenn Voyles" rounded/>
-      <h1 className="mt-5 ">About me</h1>
-      <p className="aboutText pb-5 px-5 m-4 text-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+        <Image className="aboutMePic m-5" src="./porfolio-pic2.jpg" alt="Brenn Voyles" rounded />
+        <h1 className="mt-5 ">About me</h1>
+        <p className="aboutText pb-5 px-5 m-4 text-center">
+          Brenn is a dental hygienist who has been working in
+          the industry for over a decade.
+          She enjoyed working with her patients and helping them maintain
+          their oral health, but over time, she began to feel unfulfilled
+          in her career. She found herself yearning for a creative outlet
+          and a more challenging profession that would allow her to work
+          remotely.
+
+          Brenn had always been interested in technology, and she began to teach
+          herself how to code in her free time. She started with simple HTML and
+          CSS, then moved on to JavaScript and other programming languages.
+          She was fascinated by the endless possibilities of web development
+          and how it could allow her to create beautiful and functional websites.
+
+          Eventually, Brenn made the difficult decision to leave her career in
+          dental hygiene and pursue web development full-time. She enrolled in a bootcamp program to hone her skills and gain
+          experience working on real-world projects. She spent countless hours
+          learning and practicing
+
+          Following one's passions and taking a leap of faith can lead to a
+          fulfilling and rewarding career. It's not always easy to make a career
+          change, especially after years of working in a particular field, but
+          Brenn's determination and willingness to learn has driven her in this pursuit.
+
+          Web development is a growing field, and there is a high demand for
+
+          skilled professionals who can create engaging and user-friendly websites.
+          With her experience in dental hygiene, Brenn can to bring a
+          unique perspective and approach to her work in web development,
+          which sets her apart from others in the industry.
+
+
+        </p>
       </div>
     </div>
   );
